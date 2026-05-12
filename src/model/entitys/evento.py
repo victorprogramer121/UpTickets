9d89863730e62f62ebf12743ec640076d583b654
Kleber Amaro <kleberamarosilva@gmail.com>
2026-05-12 09:57:42 -0300

    update(evento)
    Modificação na entidade evento

--- src/model/entitys/evento.py
+++ src/model/entitys/evento.py
@@ -1,42 +1,61 @@
 from datetime import datetime as dt
 
+
 class Evento:
-    def __init__(self, nomeEvento:str, local:str, idadeMIN:int, estado:str,data:dt=None ):
+    def __init__(self, id, nomeEvento: str, local: str, idadeMIN: int, estado: str, data: dt = None):
         # self.__cpf=cpf
-        self.__nomeEvento=nomeEvento
+        self.__nomeEvento = nomeEvento
         self.__local = local
-        self.__idadeMIN=idadeMIN
-        self.__estado=estado
-        self.__data=data
+        self.__idadeMIN = idadeMIN
+        self.__data = data
+        self.__id = id
 
     # @property
     # def cpf(self)->str:
     #     return self.__cpf
 
     @property
-    def nomeEvento(self)->str:
+    def nomeEvento(self) -> str:
         return self.__nomeEvento
 
     @property
-    def local(self)->str:
+    def local(self) -> str:
         return self.__cidade
+
     @property
-    def idadeMIN(self)->str:
+    def idadeMIN(self) -> str:
         return self.__idadeMIN
+
     @property
-    def data(self)->dt:
+    def data(self) -> dt:
         return self.__data
 
-    def __eq__(self, other):
-        return self.__nomeEvento==other.__nomeEvento and self.__local==other.__local, print("Evento duplicado")
-    #verificar se está correto
+    @property
+    def id(self) -> int:
+        return self.__id
 
-    def evento(self)->dict:
+    def __eq__(self, other):
+        return self.__nomeEvento == other.__nomeEvento and self.__local == other.__local, print("Evento duplicado")
+
+    # verificar se está correto
+
+    def evento(self) -> dict:
         return {
-            "nomeEvento":self.__nomeEvento,
-            "local":self.__local,
-            "idadeMIN":self.__idadeMIN,
-            "data":self.__data
+            "id": self.__id,
+            "nomeEvento": self.__nomeEvento,
+            "local": self.__local,
+            "idadeMIN": self.__idadeMIN,
+            "data": self.__data
+
         }
 
+    @staticmethod
+    def dict_to_obejct(data):
+        return Evento(
+            data["id"],
+            data["nomeEvento"],
+            data["local"],
+            data["idadeMIN"],
+            data["data"]
+        )
 
