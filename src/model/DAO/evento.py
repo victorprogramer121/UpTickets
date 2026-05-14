1d75384b90084a3aaaae51f4aa53281ded0b5f6d
Kleber Amaro <kleberamarosilva@gmail.com>
2026-05-14 09:10:41 -0300

    a

---
+++ src/model/DAO/evento.py
@@ -0,0 +1,21 @@
+from src.model.DAO.databaseDB import DatabaseDB
+
+
+class Eventos_DAO:
+
+    def __init__(self):
+        self.__conn = DatabaseDB("eventos.json")
+
+    def addEventos(self, data=dict):
+        try:
+            self.__conn.salvar(data)
+            return "Evento criado com sucesso"
+        except Exception as e:
+            raise ValueError("Não foi possivel criar o evento")
+
+    def visualizarEvento(self):
+        return self.__conn.lerArquivo
+
+
+if __name__ == '__main__':
+    e1 = Eventos_DAO(1, "klbe", "rua", "16", 15042001)
